aba4d52c1b5f10522c4bd2ca81ed0886b568eda8
sunilmanipal <39825670+sunilmanipal@users.noreply.github.com>
2021-10-25 11:24:02 +0530

    Update payloadconvertor.java

--- APIFrameworkCucumber/src/main/java/utility/payloadconvertor.java
+++ APIFrameworkCucumber/src/main/java/utility/payloadconvertor.java
@@ -11,6 +11,7 @@ public class payloadconvertor {
 		String filepath = "E:\\Selenium-Workspace\\Rest Assured\\APIFrameworkCucumber\\resource\\" + filename;
 		//String filepath = System.getProperty("user.dir")+"\\resource\\" + filename;
 		return new String(Files.readAllBytes(Paths.get(filepath)));
+		System.out.print("This is sunil");
 	}
 
 }
